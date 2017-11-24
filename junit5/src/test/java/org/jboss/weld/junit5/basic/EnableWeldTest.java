a6a1bb5f31cd7b851e6ce3a33cbe337882ec1a91
Steve Moyer <smoyer1@selesy.com>
2017-11-24 10:37:49 -0500

    Adds the @EnableWeld meta-annotation.

---
+++ junit5/src/test/java/org/jboss/weld/junit5/basic/EnableWeldTest.java
@@ -0,0 +1,24 @@
+package org.jboss.weld.junit5.basic;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.enterprise.inject.spi.BeanManager;
+import javax.inject.Inject;
+
+import org.jboss.weld.junit5.EnableWeld;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@EnableWeld
+public class EnableWeldTest {
+
+	@Inject
+	BeanManager beanManager;
+	
+	@Test
+	@DisplayName("@EnableWeld initializes the Weld CDI container")
+	void enableWeldAnnotationInitializesTheWeldContainer() {
+		assertNotNull(beanManager);
+	}
+	
+}
