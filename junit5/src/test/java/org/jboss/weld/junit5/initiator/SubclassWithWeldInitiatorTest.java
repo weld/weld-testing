790a3f39819c0f2e62fb888b8383bf7b861047f0
Chris Cooper <chris.cooper@gamma.co.uk>
2018-02-06 15:41:20 +0000

    Check for @WeldSetup fields on test superclasses

---
+++ junit5/src/test/java/org/jboss/weld/junit5/initiator/SubclassWithWeldInitiatorTest.java
@@ -0,0 +1,16 @@
+package org.jboss.weld.junit5.initiator;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.jboss.weld.junit5.initiator.bean.Foo;
+import org.junit.jupiter.api.Test;
+
+public class SubclassWithWeldInitiatorTest extends SuperclassWithWeldInitiator
+{
+	@Test
+	public void testSuperclassWeldInit()
+	{
+		final Foo foo = weld.select(Foo.class).get();
+		assertNotNull(foo);
+	}
+}
---
+++ junit5/src/test/java/org/jboss/weld/junit5/initiator/bean/Foo.java
@@ -0,0 +1,6 @@
+package org.jboss.weld.junit5.initiator.bean;
+
+public class Foo
+{
+
+}
