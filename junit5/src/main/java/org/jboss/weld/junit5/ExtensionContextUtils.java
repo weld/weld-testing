e7b97fd02dd605441b31a3db5799642bce1ad27a
Kevin Wooten <kevin@outfoxx.io>
2018-06-04 09:45:34 -0700

    Requested fixes
    * Added copyrights to all files
    * Ensure test class scope is `@Singleton` to ensure produces and similar use the same instance
    * Make all `Namespace`s created on demand for each test class instance
    * Removed empty services file

--- junit5/src/main/java/org/jboss/weld/junit5/ExtensionContextUtils.java
+++ junit5/src/main/java/org/jboss/weld/junit5/ExtensionContextUtils.java
@@ -1,3 +1,19 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2018, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jboss.weld.junit5;
 
 import org.jboss.weld.environment.se.WeldContainer;
@@ -18,13 +34,16 @@ public class ExtensionContextUtils {
     private static final String EXPLICIT_PARAM_INJECTION = "explicitParamInjection";
     private static final String WELD_ENRICHERS = "weldEnrichers";
 
-    private static final Namespace EXTENSION_NAMESPACE = Namespace.create(WeldJunit5Extension.class);
+    private static Namespace EXTENSION_NAMESPACE;
     private static Namespace TEST_NAMESPACE;
 
     /**
      * We use custom namespace based on this extension class
      */
     public static ExtensionContext.Store getExtensionStore(ExtensionContext context) {
+        if (EXTENSION_NAMESPACE == null) {
+            EXTENSION_NAMESPACE = Namespace.create(WeldJunit5Extension.class);
+        }
         return context.getStore(EXTENSION_NAMESPACE);
     }
 
--- junit5/src/main/java/org/jboss/weld/junit5/auto/TestInstanceInjectionExtension.java
+++ junit5/src/main/java/org/jboss/weld/junit5/auto/TestInstanceInjectionExtension.java
@@ -1,12 +1,37 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2018, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jboss.weld.junit5.auto;
+
+import org.jboss.weld.util.annotated.ForwardingAnnotatedType;
 
 import javax.enterprise.context.spi.CreationalContext;
 import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.AnnotatedType;
+import javax.enterprise.inject.spi.BeanManager;
 import javax.enterprise.inject.spi.Extension;
 import javax.enterprise.inject.spi.InjectionPoint;
 import javax.enterprise.inject.spi.InjectionTarget;
+import javax.enterprise.inject.spi.ProcessAnnotatedType;
 import javax.enterprise.inject.spi.ProcessInjectionTarget;
+import javax.enterprise.util.AnnotationLiteral;
+import javax.inject.Singleton;
+import java.lang.annotation.Annotation;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 
 /**
@@ -16,6 +41,7 @@ import java.util.Set;
  * NOTE: When JUnit 5 provides a test instance creation extension point this
  * should be removed and the test instance should be obtained from the
  * container via proper CDI methods.
+ * https://github.com/junit-team/junit5/issues/672
  */
 public class TestInstanceInjectionExtension implements Extension {
 
@@ -30,11 +56,43 @@ public class TestInstanceInjectionExtension implements Extension {
     TestInstanceInjectionExtension() {
     }
 
+    <T> void rewriteTestClassScope(@Observes ProcessAnnotatedType<T> pat, BeanManager beanManager) {
+
+        AnnotatedType<T> annotatedType = pat.getAnnotatedType();
+
+        if (annotatedType.getJavaClass().equals(testClass)) {
+
+            // Replace any test class's scope with @Singleton
+            Set<Annotation> annotations = annotatedType.getAnnotations().stream()
+                    .filter(annotation -> beanManager.isScope(annotation.annotationType()))
+                    .collect(Collectors.toSet());
+            annotations.add(new AnnotationLiteral<Singleton>() {});
+
+            pat.setAnnotatedType(new ForwardingAnnotatedType<T>() {
+
+                @Override
+                public AnnotatedType<T> delegate() {
+                    return annotatedType;
+                }
+
+                @Override
+                public Set<Annotation> getAnnotations() {
+                    return annotations;
+                }
+
+            });
+        }
+    }
+
     <T> void rewriteTestClassInjections(@Observes ProcessInjectionTarget<T> pit) {
 
         if (pit.getAnnotatedType().getJavaClass().equals(testClass)) {
+
             InjectionTarget<T> wrapped = pit.getInjectionTarget();
+
             pit.setInjectionTarget(new InjectionTarget<T>() {
+
+                @SuppressWarnings("unchecked")
                 @Override
                 public T produce(CreationalContext<T> creationalContext) {
                     return (T) testInstance;
@@ -65,6 +123,7 @@ public class TestInstanceInjectionExtension implements Extension {
                     wrapped.preDestroy(t);
                 }
             });
+
         }
     }
 
