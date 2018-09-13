142afabea2e50742403b6e68c5e56c82cc994441
Kai Winter <kaiwinter@gmx.de>
2018-09-13 12:49:34 +0200

    Tests for Annotation inheritance
    I put the tests (clones of the current ones + inheritance) as @Nested
    tests into one class. This was not working for ActivateScopesTest which
    is why I made an own class for this tests.

---
+++ junit5/src/test/java/org/jboss/weld/junit5/auto/ActivateScopesInheritanceTest.java
@@ -0,0 +1,62 @@
+package org.jboss.weld.junit5.auto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import javax.enterprise.context.ContextNotActiveException;
+import javax.enterprise.context.ConversationScoped;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.inject.Named;
+
+import org.jboss.weld.junit5.auto.beans.Engine;
+import org.jboss.weld.junit5.auto.beans.V6;
+import org.jboss.weld.junit5.auto.beans.V8;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ActivateScopesInheritanceTest extends BaseActivateScopesInheritanceTest {
+
+    @Produces
+    @SessionScoped
+    @Named("special")
+    V8 sessionEngine = new V8();
+
+    @Produces
+    @ConversationScoped
+    @OverrideBean
+    // V8 is annotated with @ApplicationScoped, this tells the container to use this ConversationScoped bean instead
+    V6 convoEngine = new V6();
+
+    @Test
+    @DisplayName("Test that @ActivateScopes activates the specified scopes")
+    void test(BeanManager beanManager) {
+        assertTrue(beanManager.getContext(RequestScoped.class).isActive());
+        assertTrue(beanManager.getContext(SessionScoped.class).isActive());
+    }
+
+    @Test
+    @DisplayName("Test that Engine is resolved to @SessionScoped V8")
+    void test(@Named("special") Engine engine) {
+        assertEquals(engine.getThrottle(), 0);
+    }
+
+    @Test
+    @DisplayName("Test that non-activated scopes fail")
+    void test(V6 engine) {
+        assertThrows(ContextNotActiveException.class, engine::getThrottle);
+    }
+
+}
+
+/**
+ * Base class for {@link ActivateScopesInheritanceTest} to test if annotations are inherited.
+ */
+@EnableAutoWeld
+@ActivateScopes({ SessionScoped.class, RequestScoped.class })
+class BaseActivateScopesInheritanceTest {
+    // empty, just a base class to let annotations be inherited
+}
---
+++ junit5/src/test/java/org/jboss/weld/junit5/auto/InheritanceTest.java
@@ -0,0 +1,107 @@
+package org.jboss.weld.junit5.auto;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import javax.inject.Inject;
+
+import org.jboss.weld.junit5.auto.beans.Engine;
+import org.jboss.weld.junit5.auto.beans.V8;
+import org.jboss.weld.junit5.auto.extension.AddedExtension;
+import org.jboss.weld.junit5.auto.interceptorAndDecorator.DecoratedBean;
+import org.jboss.weld.junit5.auto.interceptorAndDecorator.InterceptedBean;
+import org.jboss.weld.junit5.auto.interceptorAndDecorator.TestDecorator;
+import org.jboss.weld.junit5.auto.interceptorAndDecorator.TestInterceptor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests the inheritance of Annotations.
+ */
+public class InheritanceTest {
+
+    @EnableAutoWeld
+    @AddBeanClasses(V8.class)
+    class BaseAddBeanClassesTest {
+    }
+
+    @Nested
+    class AddBeanClassesTest extends BaseAddBeanClassesTest {
+        @Inject
+        private Engine engine;
+
+        @Test
+        @DisplayName("Test that @AddBeanClasses pulls in V8 to fulfill the injected Engine interface")
+        void test() {
+            assertNotNull(engine);
+        }
+    }
+
+    @EnableAutoWeld
+    @AddEnabledDecorators(TestDecorator.class)
+    class BaseAddDecoratorTest {
+    }
+
+    @Nested
+    class AddDecoratorTest extends BaseAddDecoratorTest {
+        @Inject
+        DecoratedBean bean;
+
+        @Test
+        public void testBeanIsDecorated() {
+            Assertions.assertNotNull(bean);
+            Assertions.assertEquals(TestDecorator.class.toString() + DecoratedBean.class.toString(), bean.ping());
+        }
+    }
+
+    @EnableAutoWeld
+    @AddExtensions(AddedExtension.class)
+    class BaseAddExtensionsTest {
+    }
+
+    @Nested
+    class AddExtensionsTest extends BaseAddExtensionsTest {
+        @Test
+        @DisplayName("Test that @AddExtensions adds the specified extensions")
+        void test() {
+            assertTrue(AddedExtension.isEnabled());
+        }
+    }
+
+    @EnableAutoWeld
+    @AddEnabledInterceptors(TestInterceptor.class)
+    class BaseAddInterceptorTest {
+    }
+
+    @Nested
+    class AddInterceptorTest extends BaseAddInterceptorTest {
+        @Inject
+        InterceptedBean bean;
+
+        @Test
+        public void testBeanIsIntercepted() {
+            Assertions.assertNotNull(bean);
+            Assertions.assertEquals(TestInterceptor.class.toString() + InterceptedBean.class.toString(), bean.ping());
+        }
+    }
+
+    @EnableAutoWeld
+    @AddPackages(Engine.class)
+    class BaseAddPackagesTest {
+    }
+
+    @Nested
+    class AddPackagesTest extends BaseAddPackagesTest {
+        @Inject
+        private V8 engine;
+
+        @Test
+        @DisplayName("Test that @AddPackages pulls in V8 (without bean defining annotation) to fulfill the injected Engine interface")
+        void test() {
+            assertNotNull(engine);
+        }
+    }
+
+}
