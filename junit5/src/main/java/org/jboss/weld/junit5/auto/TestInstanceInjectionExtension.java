0612bd02c2d80378d456a9722480399a4637dbe1
Philipp Kunz <philipp.kunz@paratix.ch>
2020-06-28 21:10:30 +0200

    simplify AnnotationRewritingAnnotatedType

--- junit5/src/main/java/org/jboss/weld/junit5/auto/TestInstanceInjectionExtension.java
+++ junit5/src/main/java/org/jboss/weld/junit5/auto/TestInstanceInjectionExtension.java
@@ -16,22 +16,18 @@
  */
 package org.jboss.weld.junit5.auto;
 
-import org.jboss.weld.util.annotated.ForwardingAnnotatedType;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import javax.enterprise.event.Observes;
-import javax.enterprise.inject.spi.AnnotatedType;
 import javax.enterprise.inject.spi.BeanManager;
 import javax.enterprise.inject.spi.Extension;
 import javax.enterprise.inject.spi.ProcessAnnotatedType;
 import javax.enterprise.inject.spi.ProcessInjectionTarget;
 import javax.enterprise.util.AnnotationLiteral;
 import javax.inject.Singleton;
-import java.lang.annotation.Annotation;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 /**
  * Extension that makes test classes appear like regular beans even though instances are created by JUnit.
@@ -42,28 +38,6 @@ public class TestInstanceInjectionExtension implements Extension {
 
     private static final AnnotationLiteral<Singleton> SINGLETON_LITERAL = new AnnotationLiteral<Singleton>() {};
 
-    private static final class AnnotationRewritingAnnotatedType<T> extends ForwardingAnnotatedType<T> {
-
-        private AnnotatedType<T> delegate;
-        private Set<Annotation> annotations;
-
-        public AnnotationRewritingAnnotatedType(AnnotatedType<T> delegate, Set<Annotation> annotations) {
-            this.delegate = delegate;
-            this.annotations = annotations;
-        }
-
-        @Override
-        public AnnotatedType<T> delegate() {
-            return delegate;
-        }
-
-        @Override
-        public Set<Annotation> getAnnotations() {
-            return annotations;
-        }
-
-    }
-
     private Map<Class<?>, ?> testInstancesByClass;
 
     TestInstanceInjectionExtension(List<?> testInstances) {
@@ -72,18 +46,10 @@ public class TestInstanceInjectionExtension implements Extension {
 
     <T> void rewriteTestClassScope(@Observes ProcessAnnotatedType<T> pat, BeanManager beanManager) {
 
-        AnnotatedType<T> annotatedType = pat.getAnnotatedType();
-
-        if (testInstancesByClass.containsKey(annotatedType.getJavaClass())) {
-
-            // Replace any test class's scope with @Singleton
-            Set<Annotation> annotations = annotatedType.getAnnotations().stream()
-                    .filter(annotation -> beanManager.isScope(annotation.annotationType()))
-                    .collect(Collectors.toSet());
-            annotations.add(SINGLETON_LITERAL);
-
-            pat.setAnnotatedType(new AnnotationRewritingAnnotatedType<>(annotatedType, annotations));
+        if (testInstancesByClass.containsKey(pat.getAnnotatedType().getJavaClass())) {
+            pat.configureAnnotatedType().add(SINGLETON_LITERAL);
         }
+
     }
 
     <T> void rewriteTestInstanceInjectionTarget(@Observes ProcessInjectionTarget<T> pit) {
