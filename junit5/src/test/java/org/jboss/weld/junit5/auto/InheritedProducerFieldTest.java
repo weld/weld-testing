93ca454346c836b12d64e92460ee00f7946748b5
Philipp Kunz <philipp.kunz@paratix.ch>
2021-01-08 10:45:38 +0100

    InheritedProducerFieldTest

---
+++ junit5/src/test/java/org/jboss/weld/junit5/auto/InheritedProducerFieldTest.java
@@ -0,0 +1,48 @@
+package org.jboss.weld.junit5.auto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import javax.enterprise.context.Dependent;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@EnableAutoWeld
+public class InheritedProducerFieldTest {
+
+    @Dependent
+    static class Foo {
+    }
+
+    static class BaseClass {
+        @Produces
+        Foo baseFooProducer = new Foo();
+    }
+
+    @Nested
+    class DontAddBeanClassesInheritedFromTestBaseClassTest extends BaseClass {
+        @Test
+        void test(BeanManager beanManager) {
+            assertEquals(0, beanManager.getBeans(Foo.class).size());
+        }
+    }
+
+    @Dependent
+    static class SubClass extends BaseClass {
+    }
+
+    @Nested
+    class DontAddBeanClassesInheritedFromBeanBaseClassTest {
+        @Inject
+        SubClass subClass;
+
+        @Test
+        void test(BeanManager beanManager) {
+            assertEquals(0, beanManager.getBeans(Foo.class).size());
+        }
+    }
+
+}
