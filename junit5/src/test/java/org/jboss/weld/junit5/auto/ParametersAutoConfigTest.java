3b6bc0647da8bc5c7ab38046abc57f755a85a532
Matej Novotny <manovotn@redhat.com>
2024-01-19 09:24:34 +0100

    @ExplicitParameterInjection is now by default inherited for nested classes

--- junit5/src/test/java/org/jboss/weld/junit5/auto/ParametersAutoConfigTest.java
+++ junit5/src/test/java/org/jboss/weld/junit5/auto/ParametersAutoConfigTest.java
@@ -2,7 +2,6 @@ package org.jboss.weld.junit5.auto;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.jboss.weld.junit5.basic.Foo;
 import org.jboss.weld.junit5.explicitInjection.Bar;
@@ -16,7 +15,7 @@ public class ParametersAutoConfigTest {
     @DisplayName("Ensure the parameters Foo and Bar are automatically included in container with no configuration")
     void test(Foo foo, Bar bar) {
         assertNotNull(bar);
-        assertNull(bar.ping());
+        assertEquals(Bar.class.getSimpleName(), bar.ping());
         assertNotNull(foo);
         assertEquals(foo.getBar(), "baz");
     }
