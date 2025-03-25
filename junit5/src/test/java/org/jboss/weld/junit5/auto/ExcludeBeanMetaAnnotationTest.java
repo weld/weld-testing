ae7bcde44a50a7c93cd5f7c50a1282e7fa0c6f71
Matej Novotny <manovotn@redhat.com>
2025-03-25 11:43:06 +0100

    Allow @ExcludeBean annotation be be a meta-annotation. Add automated test.

---
+++ junit5/src/test/java/org/jboss/weld/junit5/auto/ExcludeBeanMetaAnnotationTest.java
@@ -0,0 +1,48 @@
+package org.jboss.weld.junit5.auto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import jakarta.enterprise.inject.Produces;
+
+import org.jboss.weld.junit5.basic.Foo;
+import org.jboss.weld.junit5.basic.SomeFoo;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Ensures that @ExcludeBean can be used as meta annotation and weld-testing should still process it correctly.
+ * See also https://github.com/weld/weld-testing/issues/223
+ */
+@EnableAutoWeld
+@AddBeanClasses(SomeFoo.class)
+public class ExcludeBeanMetaAnnotationTest {
+
+    @Produces
+    @MyMetaAnnotation2
+    Foo fakeFoo = new Foo("non-baz");
+
+    @Test
+    @DisplayName("Ensure @ExcludeBean is respected even as meta-annotation")
+    void test(Foo myFoo) {
+        assertNotNull(myFoo);
+        assertEquals(myFoo.getBar(), "non-baz");
+    }
+
+    @ExcludeBean
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ ElementType.FIELD, ElementType.METHOD, ElementType.ANNOTATION_TYPE })
+    @interface MyMetaAnnotation {
+    }
+
+    @MyMetaAnnotation
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ ElementType.FIELD, ElementType.METHOD })
+    @interface MyMetaAnnotation2 {
+    }
+}
